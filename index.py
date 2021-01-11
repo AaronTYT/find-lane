0a2b7129215787a0c2b7812950d8e6b61860073c
AaronTYT <63776864+AaronTYT@users.noreply.github.com>
2021-01-11 17:08:10 +0800

    (updated) - added average_slop_intercept function

--- index.py
+++ index.py
@@ -2,6 +2,20 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+def averaged_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.ployfit((x1, x2),(y1, y2),1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit,axis=0)
+
 def canny(image):
     #convert rgb to gray scale.
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -34,6 +48,7 @@ lane_image = np.copy(image)
 canny = canny(lane_image)
 cropped_image = region_of_interest(canny)
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100,  np.array([]), minLineLength=40, maxLineGap=5)
+averaged_lines = averaged_slope_intercept(lane_image, lines)
 line_image = display_lines(lane_image, lines)   
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 cv2.imshow("result", combo_image)
