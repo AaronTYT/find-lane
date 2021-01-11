6e9ed614dea1adc3aedebf0dfdb0978be9fd753a
AaronTYT <63776864+AaronTYT@users.noreply.github.com>
2021-01-11 14:18:17 +0800

    (update) - apply GaussianBlur

--- index.py
+++ index.py
@@ -7,5 +7,8 @@ lane_image = np.copy(image)
 #convert rgb to gray scale.
 gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
 
-cv2.imshow("result", gray)
+#reduce noise by applying GaussianBlur to detect proper edges/lines
+blur = cv2.GaussianBlur(gray, (5,5),0)
+
+cv2.imshow("result", blur)
 cv2.waitKey(0)
