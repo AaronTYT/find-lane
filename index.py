c4325c96276a7f8f194ab65b12324fae999a2fd2
AaronTYT <63776864+AaronTYT@users.noreply.github.com>
2021-01-11 13:38:24 +0800

    (update) - convert rgb to gray scale of the road.png

--- index.py
+++ index.py
@@ -1,5 +1,11 @@
 import cv2
+import numpy as np
 
 image = cv2.imread('road.png')
-cv2.imshow("result", image)
+lane_image = np.copy(image)
+
+#convert rgb to gray scale.
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+
+cv2.imshow("result", gray)
 cv2.waitKey(0)
